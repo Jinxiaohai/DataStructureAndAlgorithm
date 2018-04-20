315396d28386bc89e49f4c012f295d980267c3e4
Jinxiaohai <xiaohaijin@outlook.com>
2018-04-20 13:00:12 +0800

    array list complete

---
+++ arrayListWithIterator/arrayListWithIterator.pro
@@ -0,0 +1,14 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+
+HEADERS += \
+    linearlist.h \
+    changelength1d.h \
+    myExceptions.h \
+    arraylistwithiterator.h
+
+SOURCES += \
+    main.cpp
--- arrayListWithIterator/arraylistwithiterator.h
+++ arrayListWithIterator/arraylistwithiterator.h
@@ -1,10 +1,11 @@
-﻿#ifndef ARRAYLIST_H
-#define ARRAYLIST_H
+#ifndef ARRAYLISTWITHITERATOR_H
+#define ARRAYLISTWITHITERATOR_H
 
 #include <algorithm>
+#include <iostream>
 #include <iterator>
 #include <sstream>
-
+#include <string>
 #include "changelength1d.h"
 #include "linearlist.h"
 #include "myExceptions.h"
@@ -12,50 +13,72 @@
 template <class T>
 class arrayList : public linearList<T> {
  public:
-  /** 
-   * @brief 默认的构造函数
-   * @param initialCapacity 表的初始容量
-   * 
-   * @return 
-   */
   arrayList(int initialCapacity = 10);
-  /** 
-   * @brief 拷贝构造函数
-   * @param theList  
-   * 
-   * @return 
-   */
   arrayList(const arrayList<T>& theList);
-  /** 
-   * 析构函数
-   * 删除数组的内容
-   * 
-   * @return 
-   */
   ~arrayList() { delete[] element; }
 
-  /// ADT method
-  /** 
-   * 检查listSize的大小。
-   * 
-   * 
-   * @return 
-   */
   bool empty() const { return listSize == 0; }
-  /** 
-   * 返回list的大小
-   * 
-   * 
-   * @return 
-   */
   int size() const { return listSize; }
   T& get(int theIndex) const;
   int indexOf(const T& theElement) const;
   void erase(int theIndex);
-  void insert(const int theIndex, const T& theElement);
+  void insert(int theIndex, const T& theElement);
   void output(std::ostream& out) const;
 
   int capacity() const { return arrayLength; }
+
+  /// 迭代器
+  class iterator;
+  iterator begin() { return iterator(element); }
+  iterator end() { return iterator(element + listSize); }
+
+  class iterator {
+   public:
+    typedef bidirectional_iterator_tag iterator_category;
+    typedef T value_type;
+    typedef ptrdiff_t difference_type;
+    typedef T* pointer;
+    typedef T& reference;
+
+    iterator(T* thePosition = 0) { position = thePosition; }
+
+    /// dereferencing operators
+    T& operator*() const { return *position; }
+    /// return the address of dereference operators
+    T* operator->() const { return &(operator*()); }
+
+    /// return dereference
+    iterator& operator++() {
+      ++position;
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator old = *this;
+      ++position;
+      return old;
+    }
+
+    /// return dereference
+    iterator& operator--() {
+      --position;
+      return *this;
+    }
+    iterator operator--(int) {
+      iterator old = *this;
+      --position;
+      return old;
+    }
+
+    bool operator!=(const iterator rightHand) const {
+      return position != rightHand.position;
+    }
+    bool operator==(const iterator rightHand) const {
+      return !(position != rightHand.position);
+    }
+
+   protected:
+    T* position;
+  };
 
  protected:
   void checkIndex(int theIndex) const;
@@ -68,7 +91,7 @@ template <class T>
 arrayList<T>::arrayList(int initialCapacity) {
   if (initialCapacity < 1) {
     std::ostringstream s;
-    s << "Initial capacity = " << initialCapacity << " must be > 0";
+    s << "Initial capacity = " << initialCapacity << " Must be > 0";
     throw illegalParameterValue(s.str());
   }
 
@@ -93,9 +116,8 @@ T& arrayList<T>::get(int theIndex) const {
 
 template <class T>
 int arrayList<T>::indexOf(const T& theElement) const {
-  int theIndex = static_cast<int>(
-      std::find(element, element + listSize, theElement) - element);
-
+  int theIndex =
+      static_cast<int>(find(element, element + listSize, theElement) - element);
   if (theIndex == listSize) {
     return -1;
   } else {
@@ -106,14 +128,13 @@ int arrayList<T>::indexOf(const T& theElement) const {
 template <class T>
 void arrayList<T>::erase(int theIndex) {
   checkIndex(theIndex);
-
   std::copy(element + theIndex + 1, element + listSize, element + theIndex);
-  --listSize;
+  listSize = listSize - 1;
   element[listSize].~T();
 }
 
 template <class T>
-void arrayList<T>::insert(const int theIndex, const T& theElement) {
+void arrayList<T>::insert(int theIndex, const T& theElement) {
   if (theIndex < 0 || theIndex > listSize) {
     std::ostringstream s;
     s << "index = " << theIndex << " size = " << listSize;
@@ -127,13 +148,12 @@ void arrayList<T>::insert(const int theIndex, const T& theElement) {
 
   std::copy_backward(element + theIndex, element + listSize,
                      element + listSize + 1);
-
   element[theIndex] = theElement;
-  listSize = listSize + 1;
+  ++listSize;
 }
 
 template <class T>
-void arrayList<T>::output(std::ostream& out) const {
+void arrayList<T>::output(ostream& out) const {
   std::copy(element, element + listSize, std::ostream_iterator<T>(out, "  "));
 }
 
@@ -152,4 +172,4 @@ std::ostream& operator<<(std::ostream& out, const arrayList<T>& x) {
   return out;
 }
 
-#endif  // ARRAYLIST_H
+#endif  // ARRAYLISTWITHITERATOR_H
